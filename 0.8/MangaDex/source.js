1d4fd06100d934a5f74ed1edb2435ca2585542a5
kymotsujason <kymotsujason@gmail.com>
2025-01-14 15:52:43 +0000

    Deploying to gh-pages from @ kymotsujason/community-extensions@313db38ec42add4636ab31b4528ea50bc58ca28c 🚀

--- 0.8/MangaDex/source.js
+++ 0.8/MangaDex/source.js
@@ -4140,6 +4140,8 @@ var _Sources = (() => {
           })
         );
       }
+      console.log(`@@@@@@@@@@@@@@@@@@@@@@@@@@@ ${getProxyServer(this.stateManager)}`);
+      throw new Error(`@@@@@@@@@@@@@@@@@@@@@@@@@@@ ${getProxyServer(this.stateManager)}`);
       const request = App.createRequest({
         url: `${getProxyServer(this.stateManager)}`,
         method: "HEAD"
