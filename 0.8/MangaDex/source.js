b20d86acb5aca3807f7fdc4523ea6079dca45993
kymotsujason <kymotsujason@gmail.com>
2025-01-14 23:43:52 +0000

    Deploying to gh-pages from @ kymotsujason/community-extensions@0ebfd5a339eb84ce7d4464ea03a99d0cb717ba01 🚀

--- 0.8/MangaDex/source.js
+++ 0.8/MangaDex/source.js
@@ -4065,7 +4065,6 @@ var _Sources = (() => {
         });
         const response = await this.requestManager.schedule(request, 1);
         json = typeof response.data === "string" ? JSON.parse(response.data) : response.data;
-        throw new Error(`Failed to parse json results: ${json}`);
       } else {
         const request = App.createRequest({
           url: `${this.MANGADEX_API}/at-home/server/${chapterId}${forcePort ? "?forcePort443=true" : ""}`,
@@ -4081,6 +4080,7 @@ var _Sources = (() => {
         pages = chapterDetails.dataSaver.map((x) => `${serverUrl}/data-saver/${chapterDetails.hash}/${x}`);
       } else {
         pages = chapterDetails.data.map((x) => `${serverUrl}/data/${chapterDetails.hash}/${x}`);
+        throw new Error(`Failed to parse json results: ${pages[0]}`);
       }
       return App.createChapterDetails({
         id: chapterId,
